d03304faec3372d0689c60082047114b7f2a9eb6
MarvNC <rotrobmin@gmail.com>
2023-12-18 17:23:10 -0800

    Basic export working

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,6 @@
 namespace Constants {
   export const INDEX_FILE_NAME = 'index.json';
+  export const defaultOptions: DictionaryOptions = {
+    termBankMaxSize: 10000,
+  };
 }
--- src/index.ts
+++ src/index.ts
@@ -4,57 +4,64 @@ import fs from 'fs';
 // Builders
 export * from './builders/dictionaryIndexBuilder';
 
-const defaultOptions: DictionaryOptions = {
-  termBankMaxSize: 10000,
-};
-
-type DictionaryOptions = {
-  /**
-   * The maximum number of terms in a single term/kanji bank.
-   */
-  termBankMaxSize?: number;
-  /**
-   * The file name to be used when exporting the dictionary.
-   */
-  fileName?: string;
-};
-
 export class Dictionary {
   options: DictionaryOptions;
   zip: JSZip;
   constructor(options?: DictionaryOptions) {
     this.zip = new JSZip();
     // set default options
-    this.options = { ...defaultOptions, ...options };
+    this.options = { ...Constants.defaultOptions, ...options };
   }
 
-  setIndex(index: DictionaryIndex) {
+  /**
+   * Writes the dictionary index
+   * @param index
+   * @returns
+   */
+  async setIndex(index: DictionaryIndex) {
     // Check if index instance of DictionaryIndex
     // if(!index instanceof DictionaryIndex) {
     //   throw new Error('Index is not an instance of DictionaryIndex.');
     // }
-    this.saveJsonToZip(Constants.INDEX_FILE_NAME, index);
-  }
-
-  async export(path: string) {
-    const { fileName } = this.options;
-    if (!fileName) {
-      throw new Error('No file name set.');
-    }
-    
-    const zip = this.zip;
-    
-    // Check if index.json present in zip
-    const index = await zip.file(Constants.INDEX_FILE_NAME)?.async('string');
-
-    if (!index) {
-      throw new Error('No index.json found.');
-    }
+    await this.saveJsonToZip(Constants.INDEX_FILE_NAME, index);
+    return this;
   }
 
   async saveJsonToZip(fileName: string, data: any) {
     const zip = this.zip;
     const json = JSON.stringify(data);
     zip.file(fileName, json);
+  }
+
+  /**
+   * Exports the dictionary to a zip file
+   * @param path - The directory to export the dictionary to
+   * @returns
+   */
+  async export(path: string) {
+    const { fileName } = this.options;
+    if (!fileName) {
+      throw new Error('No file name set.');
+    }
+
+    const zip = this.zip;
+
+    // Check if index.json present in zip
+    const index = await zip.file(Constants.INDEX_FILE_NAME)?.async('string');
+
+    if (!index) {
+      throw new Error('No index file was set.');
+    }
+
+    // Write zip to file
+    const buffer = await zip.generateAsync({
+      type: 'nodebuffer',
+      compression: 'DEFLATE',
+      compressionOptions: {
+        level: 9,
+      },
+    });
+
+    fs.writeFileSync(`${path}/${fileName}`, buffer);
   }
 }
