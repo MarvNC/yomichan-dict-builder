4d1121267b084f5b55451a8ca2f5db5f1503f149
MarvNC <rotrobmin@gmail.com>
2023-12-19 22:05:22 -0800

    Fix tag bank saving, improve tag bank option types

--- src/types/yomitan/tagbank.ts
+++ src/types/yomitan/tagbank.ts
@@ -6,6 +6,47 @@ type TagInformation = [
   popularityScore: number,
 ];
 
+type KanjiTagCategory =
+  /**
+   * Classification
+   */
+  | 'class'
+  /**
+   * Codepoint
+   */
+  | 'code'
+  /**
+   * Dictionary Indices
+   */
+  | 'index'
+  /**
+   * Misc (Shows up at the top)
+   */
+  | 'misc';
+
+type TagOption = {
+  /**
+   * The name of the tag.
+   */
+  name: string;
+  /**
+   * The category of the tag.
+   */
+  category: string | KanjiTagCategory;
+  /**
+   * The sorting order of the tag.
+   */
+  sortingOrder?: number;
+  /**
+   * The notes of the tag.
+   */
+  notes?: string;
+  /**
+   * The popularity score of the tag.
+   */
+  popularityScore?: number;
+};
+
 type DictionaryTagBankV3 = TagInformation[];
 
-export type { DictionaryTagBankV3, TagInformation };
+export type { DictionaryTagBankV3, TagOption, TagInformation };
