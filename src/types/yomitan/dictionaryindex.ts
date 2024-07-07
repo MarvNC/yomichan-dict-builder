c1e0437e2229dfaf5ebf1ddf8f6a9f7f79117162
MarvNC <rotrobmin@gmail.com>
2024-07-06 21:11:18 -0700

    Add source and target language dictionary metadata https://github.com/themoeway/yomitan/pull/891 https://github.com/themoeway/yomitan/pull/1009

--- src/types/yomitan/dictionaryindex.ts
+++ src/types/yomitan/dictionaryindex.ts
@@ -1,13 +1,41 @@
+import { IsoLanguageCode } from '../IsoLanguageCode';
+
+/**
+ * Information about a single tag.
+ */
+type TagInfo = {
+  category?: string;
+  order?: number;
+  notes?: string;
+  score?: number;
+};
+
+/**
+ * Represents the structure of a dictionary index file.
+ */
 type DictionaryIndexType = {
+  /** Title of the dictionary. */
   title: string;
+  /** Revision of the dictionary. This value is only used for displaying information. */
   revision: string;
+  /** Whether or not this dictionary contains sequencing information for related terms. */
   sequenced?: boolean;
+  /** Format of data found in the JSON data files. */
   format?: 1 | 2 | 3;
-  author: string;
+  /** Creator of the dictionary. */
+  author?: string;
+  /** URL for the source of the dictionary. */
   url?: string;
-  description: string;
-  attribution: string;
+  /** Description of the dictionary data. */
+  description?: string;
+  /** Attribution information for the dictionary data. */
+  attribution?: string;
+  /** Language of the terms in the dictionary. */
+  sourceLanguage?: IsoLanguageCode;
+  /** Main language of the definitions in the dictionary. */
+  targetLanguage?: IsoLanguageCode;
+  /** Frequency mode of the dictionary. */
   frequencyMode?: 'occurrence-based' | 'rank-based';
 };
 
-export type { DictionaryIndexType };
+export type { DictionaryIndexType, IsoLanguageCode, TagInfo };
